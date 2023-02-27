64f0474be364afb1b4c158171ef4de0b6d0c85df
Manvendra pratap singh <39775458+manvendrasinghrathor@users.noreply.github.com>
2023-02-27 20:57:44 +0530

    Initial commit

---
+++ SSO/SSO.h
@@ -0,0 +1,18 @@
+//
+//  SSO.h
+//  SSO
+//
+//  Created by Manvendra Pratap Singh on 27/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SSO.
+FOUNDATION_EXPORT double SSOVersionNumber;
+
+//! Project version string for SSO.
+FOUNDATION_EXPORT const unsigned char SSOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSO/PublicHeader.h>
+
+
---
+++ SSO/Test.swift
@@ -0,0 +1,14 @@
+//
+//  Test.swift
+//  SSO
+//
+//  Created by Manvendra Pratap Singh on 27/02/23.
+//
+
+import UIKit
+
+extension UIView {
+ public func debugPrintView(_ string: String) {
+        debugPrint(string)
+    }
+}
